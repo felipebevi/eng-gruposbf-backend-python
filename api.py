525a1696667e4541aaa694a064536809270c0844
Felipe Bevi <felipe.bevilacqua@cetrus.com.br>
2023-09-01 17:22:41 -0300

    inicio tratamentos basicos

--- api.py
+++ api.py
@@ -1,10 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/api/convert/<pais>/<valor>")
-def inicio(pais,valor):
-    return jsonify({"message": "API inicial: "+pais+" - valor: "+valor})
+@app.route("/api/convert/<country>/<value>")
+def inicio(country,value):
+    error = False
+    message = ""
+    
+    if error == False:
+        return jsonify({"error": False, "message": "API inicial - country: "+country+" - value: "+value})
+    else:
+        return jsonify({"error": True, "message": message})
 
+
+@app.errorhandler(404)  
+def not_found(e):
+    return jsonify({"error": True, "message": "Use URL like "+request.base_url+"api/convert/<Country>/<Value> Ex.: "+request.base_url+"api/convert/BRL/123"})
+  
+  
 if __name__ == "__main__":
     app.run()
