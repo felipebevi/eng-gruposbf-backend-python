dcf2dd942a9634f9dd8be25197d51dd7b8b6ee52
Felipe Bevi <felipe.bevilacqua@cetrus.com.br>
2023-09-01 17:33:09 -0300

    implementando lista de paises/moedas

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +8,12 @@ app = Flask(__name__)
 def inicio(country,value):
     error = False
     message = ""
+    currencies = getCurrencies()
+    if country not in currencies:
+        error = True
+        message = "All country currencies needs to respect a international convention and came from this open sourced list: https://openexchangerates.org/api/currencies.json"
+    
+    
     
     if error == False:
         return jsonify({"error": False, "message": "API inicial - country: "+country+" - value: "+value})
@@ -15,8 +23,24 @@ def inicio(country,value):
 
 @app.errorhandler(404)  
 def not_found(e):
-    return jsonify({"error": True, "message": "Use URL like "+request.base_url+"api/convert/<Country>/<Value> Ex.: "+request.base_url+"api/convert/BRL/123"})
+    return jsonify({"error": True, "messages": [
+        "How to use:",
+        "Use URL like "+request.base_url+"api/convert/<Country>/<Value>",
+        "Ex.: "+request.base_url+"api/convert/BRL/123",
+        "All country currencies needs to respect a international convention and came from this open sourced list: https://openexchangerates.org/api/currencies.json"
+    ]
+    })
   
+
+def getCurrencies(ini=False):
+    url = "https://openexchangerates.org/api/currencies.json"
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    if ini != False and ini in data_json:
+        return data_json[ini]
+    else:
+        return data_json
+
   
 if __name__ == "__main__":
     app.run()
