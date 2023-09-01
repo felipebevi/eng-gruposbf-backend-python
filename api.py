0596b9056f4e452f51b555b78362e4f60028cde3
Felipe Bevi <felipe.bevilacqua@cetrus.com.br>
2023-09-01 16:48:04 -0300

    parametros URL

--- api.py
+++ api.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def inicio():
-    return jsonify({"message": "API inicial"})
+@app.route("/api/convert/<pais>/<valor>")
+def inicio(pais,valor):
+    return jsonify({"message": "API inicial: "+pais+" - valor: "+valor})
 
 if __name__ == "__main__":
     app.run()
